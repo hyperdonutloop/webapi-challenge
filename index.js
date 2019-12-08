01f70f8553a1401d524291c07d35d9c8de476483
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-08 09:39:57 -0800

    added in comments explaining what code is doing for future reference

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./server.js');
 server.listen(5000, () => {
   console.log('\n* Server Running on http://localhost:5000 *\n');
 })
+
+// watching for connections on port 5000
+
+// configured an npm script called *server* and *start* in package.json under scripts. 
+// made nodemon a dev dependency by npm i -D nodemon
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require('express'); // importing the express package (has to be installed: npm i express)
+
 const projectsRouter = require('./projects/projectsRouter.js');
 const actionsRouter = require('./actions/actionsRouter.js');
 
-const server = express();
+const server = express(); // creates the server
 
-server.use(express.json());
-server.use('/api/projects', projectsRouter);
+server.use(express.json()); // you NEED express.json for POST and PUT requests because you are *sending data* (in the form of a data object) to the server and asking the server to accept or store that data(object), which is enclosed in the body (req.body) of that POST or PUT request.
+// This is built within express to recognize an incoming request as a JSON object.
+server.use('/api/projects', projectsRouter); // telling the server to use a specific endpoint, passing in "component" and in that component all routes have this address (base URL)
 server.use('/api/actions', actionsRouter);
 
-module.exports = server;
+module.exports = server; 
 
 // /projects to store all projects
 // /actions to store all actions
